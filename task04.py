ccf65e06a6e91692f01a561a80b3a0d3531a3794
akbaraminjonov2-tech <akbaraminjonov2@gmail.com>
2026-06-11 12:30:02 +0500

    task04.py bajarildi

---
+++ task04.py
@@ -0,0 +1,12 @@
+def get_grade(score):
+    if score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    else:
+        return "F"
+
+ball = int(input("Ball kiriting: "))
+print(get_grade(ball))
